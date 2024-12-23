181af63e27e99f9c1dd304ba6e13fe3045c307d1
beskibeski <benladen1917@inbox.ru>
2024-12-23 23:10:15 +0300

    feat: carbon and mac

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string' && typeof Number(sampleActivity) === 'number' && MODERN_ACTIVITY > +sampleActivity && +sampleActivity > 0) {
+    k = Math.log(2).toFixed(3) / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+  }
+  return false;
 }
 
 module.exports = {
