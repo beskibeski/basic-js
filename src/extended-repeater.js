d14de0e77f46c386d5fdefc0b74b4f21bf896d04
beskibeski <benladen1917@inbox.ru>
2024-12-26 23:23:07 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;  
+  return (str + ((addition + additionSeparator)
+    .repeat(additionRepeatTimes))
+    .slice(0, additionSeparator.length > 0 ? -additionSeparator.length : (addition + additionSeparator).repeat(additionRepeatTimes).length) + separator)
+    .repeat(repeatTimes)
+    .slice(0, -separator.length);
 }
 
 module.exports = {
