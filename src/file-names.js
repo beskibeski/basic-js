b9edf6e0bed2422bc15e378aca9e5d063f9fd347
beskibeski <benladen1917@inbox.ru>
2025-01-03 01:34:39 +0300

    feat: filenames, matrix-elements, recursive and sort

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const arr = names;
+  for (let i = 0; i < arr.length; i += 1) {
+    let count = 0;
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i] === arr[j]) {
+        count += 1;
+        arr[j] = `${arr[j]}(${count})`;
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const arraySortedWithoutMinus = arr.filter((element) => element !== -1).sort((a, b) => a - b);
+
+  const indexesOfMinusArray = arr.reduce((array, value, index) => {
+    if (value === -1) {
+      return [...array, index];
+    } 
+    return array;
+  }, []);
+
+  let count = 0;
+
+  return arr.map((element, index) => {
+    if (indexesOfMinusArray.includes(index)) {
+      return element;
+    } else {      
+      const sortedElement = arraySortedWithoutMinus[count];
+      count += 1;
+      return sortedElement;
+    }
+  });  
 }
 
 module.exports = {
