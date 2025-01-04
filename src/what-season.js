b81211322a1a2f7aa8274ceca6d8879b8521307b
beskibeski <benladen1917@inbox.ru>
2025-01-04 03:28:03 +0300

    feat: simple-chain and what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!'
+  };
+  if (!Date.parse(date) || date.hasOwnProperty('toString')) {
+    throw new Error('Invalid date!');
+  };
+  
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  const dateToRemake = day > 0 ? month + 1 : month;
+
+  if (dateToRemake === 12 || dateToRemake === 1 || dateToRemake === 2) {
+    return seasons[0];
+  }
+  if (dateToRemake === 3 || dateToRemake === 4 || dateToRemake === 5) {
+    return seasons[1];
+  }
+  if (dateToRemake === 6 || dateToRemake === 7 || dateToRemake === 8) {
+    return seasons[2];
+  }
+  if (dateToRemake === 9 || dateToRemake === 10 || dateToRemake === 11) {
+    return seasons[3];
+  }
 }
 
 module.exports = {
